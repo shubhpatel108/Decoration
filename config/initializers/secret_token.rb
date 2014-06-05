3a84b270d638dc77ef0c14a9c77aaf271f78730b
Shubham Patel <shubhpatel108@gmail.com>
2014-06-05 23:08:37 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Decoration::Application.config.secret_key_base = 'aef5f9a020f67d5462df60e9436fe47caa3dd5c41a9cf872d4f003ae1fc599152d79b2cd253f23efdb2dcf1180f6f21bb23929e53c0ef35f3a8e0c3dfb49f37b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Decoration::Application.config.session_store :cookie_store, key: '_Decoration_session'
